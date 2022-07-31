fa098889cd18ba2a5c9f91528d4c5dcf1e17fb24
Naheed <naheedcse@gmail.com>
2022-07-31 14:13:19 +0800

    Parallel Algorithm

--- par_src/hgrun.sh
+++ par_src/hgrun.sh
@@ -6,27 +6,22 @@ export OMP_PROC_BIND=true
 g++ -std=c++11 -fopenmp -o hgmain parallel_localcore.cpp    #ubuntu_cpp
 # g++-11 -fopenmp -o hgmain parallel_localcore.c      #mac
 
-# declare -a dset=("pref" "dblp")
-declare -a dset=("enron")
-# declare -a dset=("enron" "bin_2" "bin_5" "congress" "contact" "dblp" "pref" "aminer")
-lb=1 # Apply Load balancing or not
+declare -a dset=("enron" "bin_2" "bin_5" "congress" "contact" "dblp" "pref" "aminer")
+lb=1 # Apply Load balancing
 for data in "${dset[@]}"
     do
-    # declare -a threads=("1" "2" "4" "8" "16" "32" "64" "128" "256" "512")
-    declare -a threads=("1" "2" "4" "8")
-    # declare -a threads=("16")
+    declare -a threads=("1" "2" "4" "8" "16" "32" "64")
     for t in "${threads[@]}"
         do
             ./hgmain $data $t $lb
         done
     done 
 
-lb=0 # Apply Load balancing or not
+lb=0 # No load-balancing
 for data in "${dset[@]}"
     do
-    # declare -a threads=("1" "2" "4" "8" "16" "32" "64" "128" "256" "512")
-    declare -a threads=("1" "2" "4" "8")
-    # declare -a threads=("16")
+    declare -a threads=("1" "2" "4" "8" "16" "32" "64")
+    # declare -a threads=("1" "2" "4" "8")
     for t in "${threads[@]}"
         do
             ./hgmain $data $t $lb
