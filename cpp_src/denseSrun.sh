33eee7e2fc066b9881b4a4e0f3b13f057ee36ce4
Naheed <naheedcse@gmail.com>
2022-07-31 22:14:00 +0800

    paper

--- cpp_src/denseSrun.sh
+++ cpp_src/denseSrun.sh
@@ -3,7 +3,8 @@
 g++ -std=c++11 -Wall -g -o densmain densest_subhypergraph.cpp hypergraph.cpp readhg.h #  run on gnu c++ compiler
 
 # declare -a dset=("contact")
-declare -a dset=("klay" "protein" "bin_2" "bin_5"  "contact" "congress" "enron" "dblp" "pref" "aminer")
+declare -a dset=("protein" "klay")
+# declare -a dset=("klay" "protein" "bin_2" "bin_5"  "contact" "congress" "enron" "dblp" "pref" "aminer")
 declare -a algorithms=("nbr" "deg")
 
 for dataset in "${dset[@]}"
--- cpp_src/readhg.h
+++ cpp_src/readhg.h
@@ -13,9 +13,9 @@ std::map <std::string,std::string> dataset_to_filename = {
             {"congress" , "../data/datasets/real/congress-bills.hyp"},
             {"contact" , "../data/datasets/real/contact-primary-school.hyp"},
             {"dblp", "../data/datasets/real/DBLP.hyp"},
-            {"protein", "../data/datasets/real/humancomplexes.hyp"},
+            {"protein", "../data/datasets/protein/humancomplexes.hyp"},
             {"aminer","../data/datasets/real/aminer.hyp"},
-            {"klay","../data/datasets/real/klay_s.hyp"},           
+            {"klay","../data/datasets/kenneth_lay/klay_s.hyp"},           
            
             {"bin_2" , "../data/datasets/synthetic/binomial_5_500_4_0.200000_sample_2_iter_1.txt"},
             {"bin_5" , "../data/datasets/synthetic/binomial_5_500_3_0.200000_sample_5_iter_1.txt"},
