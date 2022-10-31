7e71ba49a77251ead5c2d461a374cabd635b950a
bishwamittra <bishwa12buet@gmail.com>
2022-10-31 16:22:57 +0800

    updated bash

--- python_src/run_diffusion_bishwa.sh
+++ python_src/run_diffusion_bishwa.sh
@@ -1,6 +1,18 @@
-python SirApplication3data.py --level 2 -a naive_nbr -d enron --num_delete 3
-python SirApplication3data.py --level 2 -a naive_degree -d enron --num_delete 3
-python SirApplication3data.py --level 2 -a graph_core -d enron --num_delete 3
+python SirApplication3data.py --level 2 -a naive_nbr -d enron --num_delete 10
+python SirApplication3data.py --level 2 -a naive_degree -d enron --num_delete 10
+python SirApplication3data.py --level 2 -a graph_core -d enron --num_delete 10
+python SirApplication3data.py --level 2 -a naive_nbr -d enron --num_delete 18
+python SirApplication3data.py --level 2 -a naive_degree -d enron --num_delete 18
+python SirApplication3data.py --level 2 -a graph_core -d enron --num_delete 18
+
+
+python run.py -sir_exp3 1 -a naive_nbr -d enron --num_delete 10
+python run.py -sir_exp3 1 -a naive_degree -d enron --num_delete 10
+python run.py -sir_exp3 1 -a graph_core -d enron --num_delete 10
+python run.py -sir_exp3 1 -a naive_nbr -d enron --num_delete 18
+python run.py -sir_exp3 1 -a naive_degree -d enron --num_delete 18
+python run.py -sir_exp3 1 -a graph_core -d enron --num_delete 18
+
 
 # python SirApplication3data.py --level 2 -a naive_nbr -d default --num_delete 400
 # python run.py -a naive_nbr -d default -sir_exp3 1 --num_delete 400
