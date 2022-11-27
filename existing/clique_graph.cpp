09eb33b91335da0693c4d7ffd1f89b34e1c96f54
Naheed <naheedcse@gmail.com>
2022-11-27 20:37:29 +0800

    parallel csr

--- existing/clique_graph.cpp
+++ existing/clique_graph.cpp
@@ -213,9 +213,11 @@ int main(int argc, char *argv[])
             std::cout<<argv[2]<<" "<<argv[3]<<"\n";
             std::cout<<"Clique graph\n";
             std::string init_type = "nbr"; // or "lub" (local upper bound)
-            h.initialise();
+            // h.initialise();
             clock_t ck_start = clock();
             getClique(h);
+            h.initialise();
+            // h.printHypergraph();
             auto ck_time = double(clock()-ck_start)/double(CLOCKS_PER_SEC);
             // std::cout <<"E-Peel\n";
             Algorithm a(h);
@@ -234,19 +236,19 @@ int main(int argc, char *argv[])
             // a.writelog();
             // a.writeNbrQ();
             // std::cout << "core: \n";
-            std::string file = "../output/core_"+a.output["algo"]+"_"+h.dataset+".csv";
-            std::stringstream ss;
-            for(auto elem: a.core)
-            {
-                ss << elem.first << "," << elem.second << "\n";
-            }
-            std::ofstream out(file.c_str());
-            if(out.fail())
-            {
-                out.close();
-            }
-            out << ss.str();
-            out.close();
+            // std::string file = "../output/core_"+a.output["algo"]+"_"+h.dataset+".csv";
+            // std::stringstream ss;
+            // for(auto elem: a.core)
+            // {
+            //     ss << elem.first << "," << elem.second << "\n";
+            // }
+            // std::ofstream out(file.c_str());
+            // if(out.fail())
+            // {
+            //     out.close();
+            // }
+            // out << ss.str();
+            // out.close();
 
         }
     }
