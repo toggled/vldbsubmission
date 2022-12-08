f741c67fac72b313f2cd44d090e0938e0f82c0ee
Naheed <naheedcse@gmail.com>
2022-12-09 00:55:10 +0800

    final run script

---
+++ par_src/finalrun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export OMP_WAIT_POLICY=active
+export OMP_DYNAMIC=true
+export OMP_PROC_BIND=true
+g++ -std=c++11 -fopenmp -o optIlocalp parallel_localcoreinitOpt.cpp    #ubuntu_cpp
+# g++-11 -fopenmp -o hgmain parallel_localcore.c      #mac
+
+declare -a dset=("enron" "bin_2" "bin_5" "congress" "contact" "dblp" "pref" "aminer")
+
+declare -a bestThreadInit=("8" "8" "8" "8" "8" "32" "8" "32")
+declare -a threads=("1" "2" "4" "8" "16" "32" "64" "128")
+
+lb=1 # Apply Load balancing
+for i in "${!dset[@]}"
+    do
+	data=${dset[i]}
+    	bestThread=${bestThreadInit[i]}
+	for t in "${threads[@]}"
+	do
+		# ./optIlocalp $data $t $lb $bestThreadInit
+		echo $data.$t.$lb.$bestThread
+	done
+    done
