4a257ef0e336d048e0bb81b187d57f0f894c3cdb
Naheed <naheedcse@gmail.com>
2022-12-09 23:02:28 +0800

    bug

--- par_src/finalrundyns.sh
+++ par_src/finalrundyns.sh
@@ -5,7 +5,7 @@ export OMP_PROC_BIND=true
 g++ -std=c++11 -fopenmp -o optIlocalpdyn parallel_localcoreinitOptdyn.cpp    #ubuntu_cpp
 # g++-11 -fopenmp -o hgmain parallel_localcore.c      #mac
 
-declare -a dset=("enron" "bin_2" "bin_5" "congress" "contact" "dblp" "pref" "aminer")
+declare -a dset=("enron")
 
 declare -a bestThreadInit=("8" "8" "8" "8" "8" "32" "8" "32")
 declare -a threads=("1" "2" "4" "8" "16" "32" "64" "128")
@@ -13,11 +13,12 @@ declare -a threads=("1" "2" "4" "8" "16" "32" "64" "128")
 lb=1 # Apply Load balancing
 for i in "${!dset[@]}"
     do
-	data=${dset[i]}
+		data=${dset[i]}
     	bestThread=${bestThreadInit[i]}
-	for t in "${threads[@]}"
-	do
-		#./optIlocalpdyn $data $t $lb $bestThread
-		echo $data.$t.$lb.$bestThread
+		for t in "${threads[@]}"
+		do
+			./optIlocalpdyn $data $t $lb $bestThread
+			# echo $data.$t.$lb.$bestThread
+		done 
 	done
-    done
+
