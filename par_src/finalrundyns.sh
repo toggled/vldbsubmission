a52177eeb565ad8de7322375f630cbba09ccd18e
Naheed <naheedcse@gmail.com>
2022-12-10 00:05:49 +0800

    par_src

--- par_src/finalrundyns.sh
+++ par_src/finalrundyns.sh
@@ -5,7 +5,7 @@ export OMP_PROC_BIND=true
 g++ -std=c++11 -fopenmp -o optIlocalpdyn parallel_localcoreinitOptdyn.cpp    #ubuntu_cpp
 # g++-11 -fopenmp -o hgmain parallel_localcore.c      #mac
 
-declare -a dset=("enron")
+declare -a dset=("enron" "bin_2" "bin_5" "congress" "contact" "dblp" "pref" "aminer")
 
 declare -a bestThreadInit=("8" "8" "8" "8" "8" "32" "8" "32")
 declare -a threads=("1" "2" "4" "8" "16" "32" "64" "128")
