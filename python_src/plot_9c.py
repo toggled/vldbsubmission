252e993086e8ad8d0c729b8b2993dbe2323e6270
bishwamittra <bishwa12buet@gmail.com>
2022-12-13 15:44:28 +0800

    updated plots

--- python_src/plot_9c.py
+++ python_src/plot_9c.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import matplotlib.ticker as ticker
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dataset", type=str, default="dblp")
@@ -12,61 +13,60 @@ args = parser.parse_args()
 
 output_folder = '../output/'
 
-fontsize = 24
+fontsize = 28
 labelsize = 20
-
+save = True
+    
 
 dataset = args.dataset
-src = '../output/'
-cols = ['algo', 'dataset', 'exp', 'intervention_results', 'max propagation time',
-        'num delete', 'p', 'result', 'seed size', 'timestep_results']
-# cols = ['dataset', 'p', 'algo', 'exp', 'result', 'timestep_results', 'intervention_results', 'num delete']
-# cols = ['dataset', 'p', 'algo', 'exp', 'result', 'timestep_results', 'intervention_results']
-
-df_nbr = pd.read_csv(
-    src + "propagation_result_recursive_delinner_" + dataset + "_naive_nbr3.csv", header=None)
-df_nbr.columns = cols
-
-df_degree = pd.read_csv(
-    src + "propagation_result_recursive_delinner_" + dataset + "_naive_degree3.csv", header=None)
-df_degree.columns = cols
-
-df_graph_core = pd.read_csv(
-    src + "propagation_result_recursive_delinner_" + dataset + "_graph_core3.csv", header=None)
-df_graph_core.columns = cols
-
-df = pd.concat([df_degree, df_nbr, df_graph_core])
-df.head(n=10)
-
-
-time_step_list = [100]
-
-sns.set(rc={'figure.figsize': (10, 5)})
-sns.set_style("whitegrid", {'axes.grid': False})
-sns.set_style("ticks")
-# plt.style.use('grayscale')
-# from matplotlib.ticker import MaxNLocator
-# sns.set(rc={'figure.figsize': (7, 4)})
-# plt.rcParams['figure.figsize'] = (7,5)
-
-lw = 3
-save = True
-output_folder = '../fig/'
-topk = 5
-
-ignore_datasets = ['bin_1', 'bin_2', 'bin_4',
-                   'bin_5', 'congress', 'contact']
-group_list = ['dataset', 'p', 'algo', 'num delete']
-goodname_algo = {
-    'graph_core': 'clique',
-    'naive_nbr': 'nbr',
-    'naive_degree': 'degree'
-}
-order = [goodname_algo[a]
-         for a in ['naive_nbr', 'graph_core', 'naive_degree']]
-
 processed_filename = "../output/processed_propagation_result_9b_" + dataset + ".csv"
+time_step_list = [100]
 if(not os.path.isfile(processed_filename)):
+
+    src = '../output/'
+    cols = ['algo', 'dataset', 'exp', 'intervention_results', 'max propagation time',
+            'num delete', 'p', 'result', 'seed size', 'timestep_results']
+    # cols = ['dataset', 'p', 'algo', 'exp', 'result', 'timestep_results', 'intervention_results', 'num delete']
+    # cols = ['dataset', 'p', 'algo', 'exp', 'result', 'timestep_results', 'intervention_results']
+
+    df_nbr = pd.read_csv(
+        src + "propagation_result_recursive_delinner_" + dataset + "_naive_nbr3.csv", header=None)
+    df_nbr.columns = cols
+
+    df_degree = pd.read_csv(
+        src + "propagation_result_recursive_delinner_" + dataset + "_naive_degree3.csv", header=None)
+    df_degree.columns = cols
+
+    df_graph_core = pd.read_csv(
+        src + "propagation_result_recursive_delinner_" + dataset + "_graph_core3.csv", header=None)
+    df_graph_core.columns = cols
+
+    df = pd.concat([df_degree, df_nbr, df_graph_core])
+    df.head(n=10)
+
+
+    
+
+    # plt.style.use('grayscale')
+    # from matplotlib.ticker import MaxNLocator
+    # sns.set(rc={'figure.figsize': (7, 4)})
+    # plt.rcParams['figure.figsize'] = (7,5)
+
+    lw = 3
+    output_folder = '../fig/'
+    topk = 5
+
+    ignore_datasets = ['bin_1', 'bin_2', 'bin_4',
+                    'bin_5', 'congress', 'contact']
+    group_list = ['dataset', 'p', 'algo', 'num delete']
+    goodname_algo = {
+        'graph_core': 'clique',
+        'naive_nbr': 'nbr',
+        'naive_degree': 'degree'
+    }
+    order = [goodname_algo[a]
+            for a in ['naive_nbr', 'graph_core', 'naive_degree']]
+
     df_plot = None
     for key, item in df[(df['intervention_results'].notnull())].groupby(group_list, as_index=False):
 
@@ -112,30 +112,44 @@ else:
     df_plot = pd.read_csv(processed_filename)
 
 for time_step in time_step_list:
+
+    final_legend_dic = {
+        'nbr': 'Nbr',
+        'degree': 'Degree',
+        'clique': 'Clique',
+        'kd': '(k, d)',
+        "dk": '(d, k)'
+    }
+    df_plot['Decomposition'] = df_plot['Decomposition'].replace(final_legend_dic)
+
+
     # assert len(df_plot['core number sorted'].unique()) == 1
+    fig, ax = plt.subplots(figsize=(8, 4))
     sns.barplot(y='infected difference', x='num delete',
-                hue='Decomposition', data=df_plot[df_plot['time_step'] == time_step])
-    plt.xlabel('#deleted nodes from inner core', fontsize=fontsize)
-    plt.ylabel("Decrease in avg.\nspread per seed", fontsize=fontsize)
-    plt.xticks(fontsize=fontsize)
-    plt.yticks(fontsize=fontsize)
+                hue='Decomposition', hue_order=['Clique', 'Degree', 'Nbr'], data=df_plot[df_plot['time_step'] == time_step])
+    plt.xlabel('#deleted nodes from inner core', fontsize=fontsize - 2)
+    plt.ylabel("Decrease in avg.\nspread per seed", fontsize=fontsize - 2)
+    ax.xaxis.set_major_formatter(ticker.EngFormatter())
+    plt.xticks(fontsize=fontsize - 2)
+    plt.yticks(fontsize=fontsize - 2)
     plt.grid(axis="y")
-    plt.legend(loc='best', fontsize=fontsize-4)
+    # plt.legend(loc='best', fontsize=fontsize-4)
+    plt.legend(loc='upper center', bbox_to_anchor=(
+        0.5, 1.3), ncol=4, fancybox=False, shadow=True, fontsize=labelsize-2, columnspacing=0.8)
     # plt.title(dataset + ", #delete: " + str(num_delete) +", time:" + str(time_step), fontsize=fontsize - 4)
-    plt.title(dataset + ", " + "time: " +
-              str(time_step), fontsize=fontsize - 4)
+    # plt.title(dataset + ", " + "time: " + str(time_step), fontsize=fontsize - 4)
     # leg = plt.legend(loc="best", fontsize=fontsize-6,
     #                 frameon=False, bbox_to_anchor=(0.44, 0.52))
     # for legobj in leg.legendHandles:
     #     legobj.set_linewidth(4.0)
 
     plt.tight_layout()
-    filename = dataset + "_9c_" + str(time_step)
+    filename = "../fig/" + dataset + "_9c_" + str(time_step) + ".pdf"
     print(filename)
     # print(output_folder)
     if(save):
-        plt.savefig(output_folder + filename + ".pdf")
-        # plt.show()
+        plt.savefig(filename)
+        plt.show()
         pass
     else:
         # print(filename)
